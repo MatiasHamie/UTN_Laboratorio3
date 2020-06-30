6eaf7bebcaa7bcd91c0566ea146e61f5d686b4f3
MatiasHamie <mnhamie@gmail.com>
2020-06-30 18:19:15 -0300

    agrego segundo parcial y practica parcial

---
+++ Hamie.SPLABIII3D/Parcial/js/entidades.js
@@ -0,0 +1,28 @@
+class Anuncio {
+    id;
+    titulo;
+    transaccion;
+    descripcion;
+    precio;
+
+    constructor(id, titulo, transaccion, descripcion, precio){
+        this.id = id;
+        this.titulo = titulo;
+        this.transaccion = transaccion;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+}
+
+export class Anuncio_Auto extends Anuncio {
+    num_puertas;
+    num_KMs;
+    potencia;
+
+    constructor(id, titulo, transaccion, descripcion, precio, num_puertas, num_KMs, potencia){
+        super(id, titulo, transaccion, descripcion, precio);
+        this.num_puertas = num_puertas;
+        this.num_KMs = num_KMs;
+        this.potencia = potencia;
+    }
+}
